1c4010771e88bd9857a58bb3cfdcb6861c119145
Tom Lea <commit@tomlea.co.uk>
2011-05-10 23:47:53 +0100

    Store the content-type.

--- lib/justpics.rb
+++ lib/justpics.rb
@@ -20,19 +20,26 @@ class Justpics < Sinatra::Base
       return "No file selected"
     end
     id = Digest::SHA1.file(tmpfile.path).hexdigest
-    id
 
-    AWS::S3::S3Object.store(id, tmpfile, 'justpics')
+    AWS::S3::S3Object.store(id, tmpfile, 'justpics', :content_type => params[:file][:type])
     redirect "/#{id}"
   end
 
   get "/:id" do
-    content_type "*/*"
-    response['Cache-Control'] = "public, max-age=#{60*60*24*356*3}"
-    response['ETag'] = "the-same-thing-every-time"
-    response['Last-Modified'] = Time.at(1337).httpdate
     id = params[:id].to_s[0...40]
-    AWS::S3::S3Object.value(id, 'justpics')
+    begin
+      file = AWS::S3::S3Object.find(id, 'justpics')
+      content_type file.content_type
+
+      response['Cache-Control'] = "public, max-age=#{60*60*24*356*3}"
+      response['ETag'] = "the-same-thing-every-time"
+      response['Last-Modified'] = Time.at(1337).httpdate
+
+      file.value
+    rescue AWS::S3::NoSuchKey
+      status 404
+      "Not here"
+    end
   end
 
   class AlwaysFresh
@@ -41,7 +48,8 @@ class Justpics < Sinatra::Base
     end
 
     def call(env)
-      if env["HTTP_IF_MODIFIED_SINCE"] or env["HTTP_IF_NONE_MATCH"]
+      p env
+      if (env["HTTP_IF_MODIFIED_SINCE"] or env["HTTP_IF_NONE_MATCH"]) and env["REQUEST_METHOD"] == "GET" and env["REQUEST_PATH"].length > 40
         [304, {}, []]
       else
         @app.call(env)
